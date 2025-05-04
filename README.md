bfe483b1561b5b5aeae5e6af91d1a98035d9c36e
VoNhtHoang <22520481@gm.uit.edu.vn>
2025-05-04 05:58:53 +0000

    Done preprocessing dataset

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 
 | Họ Và Tên | Mã số sinh viên |
 |Võ Nhật Hoàng| 22520481 |
-|||
-|||
-|||
+|Võ Nguyễn Thái Học| 22520489|
+|Nguyễn Thanh Hưng||
+|Đinh Quốc Huy||
---
+++ processing_Data_chunk_loader.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, Normalizer
+import hashlib
+from sklearn.preprocessing import Normalizer, StandardScaler
+from sklearn.pipeline import Pipeline
+import ipaddress
+import os
+
+# Khởi tạo scaler
+scaler = StandardScaler()
+normalizer = Normalizer()
+
+# Cấu hình
+chunk_size = 1000000
+input_file = "C:/Users/hoang/FileCSV_DACN_2025/bigTemp.csv"
+output_file = "C:/Users/hoang/FileCSV_DACN_2025/IoT23.csv"
+
+cols_to_drop = ['uid', 'ts', 'tunnel_parents', 'local_orig', 'local_resp']
+ip_to_float_Cols = ['id.orig_h', 'id.resp_h']
+str_to_float_Cols = ['conn_state', 'history', 'conn_state','id.resp_p', 'id.orig_p']
+numeric_cols = ['duration', 'orig_bytes', 'resp_bytes', 'missed_bytes', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes']
+onehot_cols = ['proto', 'service']
+
+label_mapping = {"benign": 0, "Benign": 0, "Malicious": 1, "malicious": 1}
+
+
+##### FUNC ############
+def ip_to_float(ip):
+    try:
+        return float(int(ipaddress.IPv4Address(ip)))/1e9
+    except:
+        return 0.0  # for invalid or empty IPs
+    
+def sum_of_squares(partition):
+    return pd.Series([(partition ** 2).sum()])
+
+def string_to_float(s):
+    if pd.notna(s):
+        return int(hashlib.sha256(str(s).encode('utf-8')).hexdigest(), 16) % 10**8 / 1e8
+    return 0
+##### FUNC ##############
+
+proto_categories =set()
+service_categories = set()
+
+for index, chunk in enumerate(pd.read_csv(input_file, chunksize=chunk_size, dtype='str')):
+    chunk.replace({"(empty)": '0', "-": '0', "":'0'}, inplace=True)
+    chunk.drop(columns=cols_to_drop, inplace=True, errors='ignore')
+    chunk.drop_duplicates(inplace=True)
+    
+    for col in numeric_cols:
+        chunk[col]=chunk[col].astype("float32")
+        
+    scaler.partial_fit(chunk[numeric_cols])  # Fit theo feature
+    
+    proto_categories.update(chunk['proto'].unique())
+    service_categories.update(chunk['service'].unique())
+    
+    print(index)
+    
+proto_categories = sorted(proto_categories)
+service_categories = sorted(service_categories)
+
+first_chunk = True
+for index, chunk in enumerate(pd.read_csv(input_file, chunksize=chunk_size, dtype='str')):
+    chunk.replace({"(empty)": '0', "-": '0', "":'0', r'[N|n][a|A][N|n]':'0', 'unknown':'0'}, inplace=True)
+    chunk.drop(columns=cols_to_drop, inplace=True, errors='ignore')
+    chunk.drop_duplicates(inplace=True)
+    
+    chunk['label'] = chunk['label'].map(label_mapping).fillna(-1).astype("int32")
+
+    chunk.fillna(0)
+
+    for col in ip_to_float_Cols:
+        chunk[col] = chunk[col].apply(ip_to_float).astype("float32")
+    for col in str_to_float_Cols:
+        chunk[col] = chunk[col].apply(string_to_float).astype("float32")
+    for col in numeric_cols:
+        chunk[col]=chunk[col].astype("float32")
+    
+    chunk['proto'] = pd.Categorical(chunk['proto'], categories=proto_categories)
+    chunk['service'] = pd.Categorical(chunk['service'], categories=service_categories)
+    
+    chunk = pd.get_dummies(chunk, columns = onehot_cols, prefix = onehot_cols, dtype='int32')
+
+    chunk.drop(columns=[c for c in chunk.columns if 'service_-' or 'service_0' or 'proto_-' in c], errors='ignore', inplace=True)
+    ################ Scaler    
+    scaled = scaler.transform(chunk[numeric_cols])
+    normalized = normalizer.transform(scaled)
+    del scaled
+    chunk[numeric_cols] = pd.DataFrame(normalized, columns=numeric_cols)
+    del normalized
+    
+    chunk.to_csv(output_file, mode='w' if first_chunk else 'a', header=first_chunk, index=False)
+    first_chunk = False
+    print(f"Index: {index} Chunktail: {chunk.tail()}")
